f6753ef6468dc060aec10328d4bbf84865cd650f
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2025-02-26 16:54:23 -0300

    better table styles

--- packages/core/src/editor/extensions/table/index.ts
+++ packages/core/src/editor/extensions/table/index.ts
@@ -12,26 +12,46 @@ export const TableExtension = Table.extend({
       },
     };
   },
+
+  addGlobalAttributes() {
+    return [
+      {
+        types: ['tableCell', 'tableHeader'],
+        attributes: {
+          // Add selected state attribute
+          selected: {
+            default: false,
+            parseHTML: (element) => element.dataset.selected === 'true',
+            renderHTML: (attributes) => ({
+              'data-selected': attributes.selected,
+              class: attributes.selected ? 'mly-bg-blue-50' : '',
+            }),
+          },
+        },
+      },
+    ];
+  },
 }).configure({
   resizable: true,
   allowTableNodeSelection: true,
+  lastColumnResizable: true,
 });
 
 export const TableRowExtension = TableRow.configure({
   HTMLAttributes: {
-    class: 'mly-border mly-border-gray-200',
+    // class: 'mly-border mly-border-gray-200',
   },
 });
 
 export const TableHeaderExtension = TableHeader.configure({
   HTMLAttributes: {
-    class:
-      'mly-border mly-border-gray-200 mly-bg-gray-100 mly-p-2 mly-text-left',
+    // class:
+    // 'mly-border mly-border-gray-200 mly-bg-gray-100 mly-p-2 mly-text-left hover:mly-bg-gray-50',
   },
 });
 
 export const TableCellExtension = TableCell.configure({
   HTMLAttributes: {
-    class: 'mly-border mly-border-gray-200 mly-p-2',
+    // class: 'mly-border mly-border-gray-200 mly-p-2 hover:mly-bg-gray-50',
   },
 });

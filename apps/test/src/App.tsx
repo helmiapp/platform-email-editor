0c939fba3425ba13ac0f461a2e8497976583578f
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2025-02-26 11:39:06 -0300

    basic setup

---
+++ apps/test/src/App.tsx
@@ -0,0 +1,23 @@
+import { Editor } from '@maily-to/core';
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center p-10">
+      <div className="bg-background text-foreground h-full w-full max-w-full rounded-lg border border-red-500 p-10">
+        <Editor
+          config={{
+            hasMenuBar: false,
+            contentClassName:
+              '!w-full !max-w-full !bg-background !text-foreground ',
+            bodyClassName:
+              '!p-0 !w-full !border-0 !m-0 !rounded-none !bg-background',
+            toolbarClassName: '!bg-background',
+            wrapClassName: ' !w-full !bg-background',
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,10 +2,6 @@ import './globals.css';
 
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
-import { Toaster } from 'sonner';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import { TopLoader } from '@/components/top-loader';
-import { config } from '@/lib/config';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -72,10 +68,7 @@ export default function RootLayout(props: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TopLoader />
         {children}
-        <Toaster richColors />
-        <GoogleAnalytics gaId={config.googleTrackingId} />
       </body>
     </html>
   );
